3ddb955ed4024ebded6a355bf11be672cd598916
Srujan GV <srujangv05@gmail.com>
2022-05-05 18:56:33 +0530

    Jump ahead 2

---
+++ app/src/main/java/com/zephgv/mad/beastfitness/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.zephgv.mad.beastfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        textView = findViewById(R.id.haveAccount);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
