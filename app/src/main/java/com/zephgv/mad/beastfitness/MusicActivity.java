ea9d24dfd05c083134481229edac391b019f633d
Srujan GV <srujangv05@gmail.com>
2022-05-13 11:31:43 +0530

    Music activity added

--- app/src/main/java/com/zephgv/mad/beastfitness/MusicActivity.java
+++ app/src/main/java/com/zephgv/mad/beastfitness/MusicActivity.java
@@ -2,10 +2,16 @@ package com.zephgv.mad.beastfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MusicActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,17 @@ public class MusicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        button = findViewById(R.id.playbtn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUri("https://open.spotify.com/playlist/1Pw57C5UiYrDSxa21UBWWY");
+            }
+        });
+    }
 
+    private void gotoUri(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
