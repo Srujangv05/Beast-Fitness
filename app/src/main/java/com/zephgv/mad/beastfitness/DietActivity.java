19e6fcd264926cb386ed71b4c99fbc130b27d4ca
Srujan GV <srujangv05@gmail.com>
2022-07-13 09:33:58 +0530

    Profile fragment & diet updated

--- app/src/main/java/com/zephgv/mad/beastfitness/DietActivity.java
+++ app/src/main/java/com/zephgv/mad/beastfitness/DietActivity.java
@@ -3,9 +3,16 @@ package com.zephgv.mad.beastfitness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class DietActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,8 @@ public class DietActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diet);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        webView = findViewById(R.id.webview1);
+        webView.loadUrl("https://www.bodybuilding.com/recipes");
 
     }
 }
